06cc3268bc38e1d199a852107c9bfb1e25d0d164
Elbek <elbekmoldabaev07@gmail.com>
2024-01-12 00:32:03 +0600

    GN1.cpp

---
+++ GN1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    //int game objects
+    while(window.isOPen())
+    {
+        //
+        //
+        window.clear(sf::Color::Red);
+        //Render game objects
+        window.display();
+    }
+    return 0;
+}
